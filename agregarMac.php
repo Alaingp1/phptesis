a02ab04ee4bbf47a0c675ab691a81119967e3193
Daniel Rojas <cld.rojasz@gmail.com>
2021-11-20 00:08:49 -0300

    recover last id correctly

--- agregarMac.php
+++ agregarMac.php
@@ -4,14 +4,14 @@
 $idCultivo = $_POST['Cultivo_id'];
 $Sensores_nombre = $_POST['Sensores_nombre'];
 
-$sql = "INSERT INTO sensores(Sensores_estado, Sensores_nombre)  values (1,$Sensores_nombre);";
+$sql = "INSERT INTO sensores(Sensores_estado, Sensores_nombre)  values (1,'$Sensores_nombre');";
 
 $query = $mysqli->query($sql);
 $mysqli -> query($sql);
-echo "New record has id: " . $mysqli -> insert_id;
+
 $last = $mysqli -> insert_id;
 
 print("ultimo: ". $last);
-$mysqli->query("UPDATE Cultivo SET Sensores_id = $mysqli -> insert_id where Cultivo_id = $idCultivo");
+$mysqli->query("UPDATE Cultivo SET Sensores_id = $last where Cultivo_id = $idCultivo");
 
 ?>
