c90925de3b765b51ad8c8c1a69b744f430ff27a1
Daniel Rojas <cld.rojasz@gmail.com>
2021-11-19 22:07:27 -0300

    database now point to heroku

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
     
-    $mysqli = new mysqli("localhost","root","","db_indostatuspruebas");
+    $mysqli = new mysqli("us-cdbr-east-04.cleardb.com","b1754fb2e2045a","028c0b8f","heroku_5201e2f44bc5d2f");
     $mysqli->set_charset("utf8");
 
     if($mysqli->connect_errno) {
