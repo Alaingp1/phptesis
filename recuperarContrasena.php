bd234aa91fe602b7709a995e81331f2c8adca861
Sbrev97 <crew97a@gmail.com>
2021-11-22 00:52:13 -0300

    hola

--- recuperarContrasena.php
+++ recuperarContrasena.php
@@ -3,14 +3,15 @@
     require "conexion.php";
 
 
-    $Usuario_correo = $_GET['Usuario_correo'];
-    $Usuario_contrasena = password_hash($_POST['Usuario_contrasena'], PASSWORD_DEFAULT);
+    $Usuario_correo = $_REQUEST['Usuario_correo'];
+    $Usuario_contrasena = password_hash($_REQUEST['Usuario_contrasena'], PASSWORD_DEFAULT);
    
  
 
     $sql = "UPDATE usuario SET  Usuario_contrasena = '".$Usuario_contrasena."'  WHERE Usuario_correo = '".$Usuario_correo."'";
     
 
+   
    
     $query = $mysqli->query($sql);
 
