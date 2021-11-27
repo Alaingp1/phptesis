679e10ec5d1603802ca2a78828b0466c13a80596
Sbrev97 <crew97a@gmail.com>
2021-11-27 19:46:17 -0300

    restorw inicial de obtener cultivo

--- ObtenerCultivo.php
+++ ObtenerCultivo.php
@@ -3,12 +3,7 @@
     require "conexion.php";
 
     
-        
-<<<<<<< HEAD
-    $sql = "SELECT c.Cultivo_apodo, c.Cultivo_imagen, tc.Tipo_nombre ,c.Cultivo_id , s.Sensores_nombre FROM cultivo AS c INNER JOIN Tipo_cultivo AS tc ON c.Tipo_id = tc.Tipo_id INNER JOIN sensores AS s ON s.Sensores_id = c.Sensores_id WHERE Cultivo_estado = 1  AND Usuario_id =  " . $_GET["Usuario_id"];
-=======
-    $sql = "SELECT c.Cultivo_apodo, c.Cultivo_imagen, tc.Tipo_nombre ,c.Cultivo_id , s.Sensores_nombre FROM cultivo AS c INNER JOIN Tipo_cultivo AS tc ON c.Tipo_id = tc.Tipo_id INNER JOIN sensores AS s ON s.Sensores_id = c.Sensores_id WHERE c.Cultivo_estado = 1 AND c.Usuario_id =  " . $_GET["Usuario_id"];
->>>>>>> b512fbbafe8fff2d22228ebb93e0a0030006318c
+    $sql = "SELECT c.Cultivo_apodo, c.Cultivo_imagen, tc.Tipo_nombre ,c.Cultivo_id  FROM cultivo AS c INNER JOIN Tipo_cultivo AS tc ON c.Tipo_id = tc.Tipo_id WHERE c.Cultivo_estado = 1 AND c.Usuario_id =  " . $_GET["Usuario_id"];
 
 
 
@@ -23,7 +18,3 @@
     echo json_encode($datos);
    
     /*SELECT c.Cultivo_apodo, c.Cultivo_imagen, tc.Tipo_nombre FROM cultivo AS c INNER JOIN Tipo_cultivo AS tc ON c.Tipo_id = tc.Tipo_id WHERE Usuario_id =  " . $_GET["Usuario_id"]*/
-
-?>
-
- 
